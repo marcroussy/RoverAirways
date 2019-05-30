c01796f081c3e0df5f8f729d1ba20a9dc588e759
Marc Roussy <mroussy@hotmail.com>
2019-05-29 20:19:33 -0400

    Remove unused code

--- Flights/Startup.cs
+++ Flights/Startup.cs
@@ -14,28 +14,7 @@ namespace Flights
     {
         public override void Configure(IFunctionsHostBuilder builder)
         {
-
-            // Register MyServiceA as transient.
-            // A new instance will be returned every
-            // time a service request is made
-            //builder.Services.AddTransient<MyServiceA>();
-
-            // Register MyServiceB as scoped.
-            // The same instance will be returned
-            // within the scope of a function invocation
-            //builder.Services.AddScoped<MyServiceB>();
-
-            // Register ICommonIdProvider as scoped.
-            // The same instance will be returned
-            // within the scope of a function invocation
-            //builder.Services.AddScoped<ICommonIdProvider, CommonIdProvider>();
-
-
-            // Register IGlobalIdProvider as singleton.
-            // A single instance will be created and reused
-            // with every service request
             builder.Services.AddSingleton<IFlightStore, FlightStore>();
-
             builder.Services.AddSingleton<IWarningGenerator, EmailWarningGenerator>();
 
         }
